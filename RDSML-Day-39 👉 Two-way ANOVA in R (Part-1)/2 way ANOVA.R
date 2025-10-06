dfb40e15c203a29dc91509c6d422c6f6cb84dd76
nbictvirtuallab-1 <refayetkarim521@gmail.com>
2025-10-07 00:12:17 +0600

    update

---
+++ RDSML-Day-39 👉 Two-way ANOVA in R (Part-1)/2 way ANOVA.R
@@ -0,0 +1,23 @@
+# Installing Packages 
+
+#install.packages(c("tidyverse", "car", "emmeans", "effectsize", "performance", "broom", "kableExtra"))
+
+library(tidyverse)
+
+# Importing the dataset
+
+d = read.csv("cropdata.csv")
+
+# Quick Exploratory Descriptive Analysis
+
+summarization = d %>%
+  group_by(fert, irr) %>%
+  summarise(mean = mean(y), se = sd (y)/ sqrt(n()), .groups = "drop")
+
+# Quick data summary visualization
+
+library(ggplot2)
+ggplot(summarization, aes(irr , mean , group = fert)) +
+  geom_line() +
+  geom_point(size = 2) +
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = 0.2)
