932e7924045ea645ee20478f3cf452cec9c61c86
nbictvirtuallab-1 <refayetkarim521@gmail.com>
2025-12-05 19:11:19 +0600

    day54 updated

--- RDSML-Day-54 👉 Support Vector Regression in R (Part-2)/SVR.R
+++ RDSML-Day-54 👉 Support Vector Regression in R (Part-2)/SVR.R
@@ -44,10 +44,39 @@ indices <- seq_len(n)
 
 train_idx <- sample(indices, size = 0.7*n)
 
-tranin_set <- bike[train_idx, ]
+train_set <- bike[train_idx, ]
 
 # column er ghor faka rakhlam mane sob koita column thakbe
 
 test_set <- bike[-train_idx, ]
 
 # minus - diye train_idx theke bad diye ja thakbe seta diye test det hobe
+
+# Scale numeric features
+
+# Identify numeric predictors
+
+num_cols <- c("temp", "atemp", "hum", "windspeed")
+
+# Computing scaling parameters on training data
+
+train_means <- sapply(train_set[, num_cols], mean)
+
+train_sds <- sapply(train_set[, num_cols], sd)
+
+# Scale Function
+# 4 ta individual column er value theke mean bad diye
+#sd diye vag korbo,tai ei function
+#last e df dilam jeno full dataframe cover kore
+
+scale_num <- function(df){
+  df[, num_cols] <-sweep(df[, num_cols], 2, train_means, "-")
+  df[, num_cols] <-sweep(df[, num_cols], 2, train_sds, "/")
+}
+
+# train_set ke df er moddhe dhukiye dite hobe
+
+train_scaled <- scale_num(train_set)
+test_scaled <- scale_num(test_set)
+
+
